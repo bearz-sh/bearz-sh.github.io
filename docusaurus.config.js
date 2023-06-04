cba6d56bfa1a7438be701b8a3fd1d7aa4d4427c9
bearz-me <119234633+bearz-me@users.noreply.github.com>
2023-06-04 01:42:30 -0500

    add missing hyphen for project name in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bearz-sh', // Usually your GitHub org/user name.
-  projectName: 'bearzsh.github.io', // Usually your repo name.
+  projectName: 'bearz-sh.github.io', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
